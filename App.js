d3c19717e2dc87b25eb3db8df487e949455e2eab
Shashwat-A <sendmail2ruchi@gmail.com>
2021-05-26 14:56:06 +0530

    added the textInput and tab navigator

--- App.js
+++ App.js
@@ -1,39 +1,41 @@
-import React from 'react';
+import * as React from 'react';
 import { StyleSheet, Text, View , Image } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import ReadStory from './screens/ReadStory';
 import WriteStory from './screens/WriteStory';
 
-export default function App() {
-  return (
-    <View>
-      <AppContainer/>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+        <AppContainer/>
+    )
+  }
 }
 
-const navTabs = createBottomTabNavigator({
-  Write : {screen: WriteStory},
+const tabNavigator = createBottomTabNavigator({
+  Write: {screen: WriteStory},
   Read : {screen: ReadStory}
-  },
+}, 
 
   {defaultNavigationOptions: ({navigation}) => ({
-    tabBarIcon: ()=> {
-      const route = navigation.state.routeName;
-      if(route === "Write") {
+    tabBarIcon: () => {
+      const nameRoot = navigation.state.routeName;
+      if(nameRoot === "Write") {
         return(
-          <Image source={require("./assets/write.png")} style={{width: 40, height: 40}}/>
+          <Image source = {require("./assets/write.png")} style={{width: 40, height: 40}}/>
         )
-        } else if(route === "Read") {
+      } else if(nameRoot === 'Read') {
+        return(
           <Image source={require("./assets/read.png")} style={{width: 40, height: 40}}/>
-        }
+        )
       }
-    })
+    }
+  })
   }
 )
 
-const AppContainer = createAppContainer(navTabs)
+const AppContainer = createAppContainer(tabNavigator)
 
 const styles = StyleSheet.create({
   text: {
--- screens/WriteStory.js
+++ screens/WriteStory.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default class WriteStory extends Component {
     render() {
         return(
             <View style={styles.container}>
-                <Text style={styles.text}>WriteStory</Text>
+                <TextInput
+                    placeholder= "Story Title"
+                    style={styles.namBx}
+                />
+
+                <TextInput
+                    placeholder= "Author"
+                    style={styles.authBx}
+                />
+
+                <TextInput
+                    placeholder= "Write your Story"  Y
+                    multiline= {true}
+                    style={styles.writeBx}
+                />
+
             </View>
         )
     }
@@ -23,5 +38,36 @@ const styles = StyleSheet.create({
         fontSize: 20,
         textAlign: 'center',
         fontWeight: 'bold'
+    },
+
+    namBx : {
+        justifyContent: 'center',
+        textAlign: 'center',
+        fontSize: 24,
+        width : '80%',
+        margin: 30,
+        borderWidth: 2,
+        height: '20%',
+    },
+
+    authBx : {
+        justifyContent: 'center',
+        textAlign: 'center',
+        fontSize: 24,
+        width : '80%',
+        margin: 50,
+        borderWidth: 2,
+        height: '20%',
+    },
+
+    writeBx : {
+        justifyContent: 'center',
+        textAlign: 'left',
+        alignItems: 'center',
+        fontSize: 24,
+        width : '80%',
+        margin: 30,
+        borderWidth: 2,
+        height: '50%',
     }
   });
